9c3b4213b07d1a0bad59260085fa8b1210833564
KyleOlhausen <olhausen.kyle43@gmail.com>
2022-12-14 16:53:37 -0600

    added createFooter function

--- src/website.js
+++ src/website.js
@@ -43,3 +43,49 @@ function createHeader() {
     nav.appendChild(contactBtn)
     header.appendChild(title)
 }
+
+function createInfo() {
+
+}
+
+function createFooter() {
+    const footer = document.createElement("footer");
+    const locContainer = document.createElement("div");
+    const hrsContainer = document.createElement("div");
+
+    const locTitle = document.createElement("h4");
+    const locLine1 = document.createElement("p");
+    const locLine2 = document.createElement("p");
+    const locLine3 = document.createElement("p");
+
+    locTitle.textContent = "Location"
+    locLine1.textContent = "936 Kiehn Route"
+    locLine2.textContent = "West Ned, TN 11230"
+    locLine3.textContent = "281-330-8004"
+
+
+    const hrsTitle = document.createElement("h4");
+    const hrsLine1 = document.createElement("p");
+    const hrsLine2 = document.createElement("p");
+    const hrsLine3 = document.createElement("p");
+    const hrsLine4 = document.createElement("p");
+
+    hrsTitle.textContent = "Hours"
+    hrsLine1.textContent = "Monday - Thursday 11am to 9pm"
+    hrsLine2.textContent = "Friday 11am to 9pm"
+    hrsLine3.textContent = "Saturday 10am to 10pm"
+    hrsLine4.textContent = "Sunday 10am to 9pm"
+
+    footer.appendChild(locContainer);
+    footer.appendChild(hrsContainer);
+    locContainer.appendChild(locTitle);
+    locContainer.appendChild(locLine1);
+    locContainer.appendChild(locLine2);
+    locContainer.appendChild(locLine3);
+    hrsContainer.appendChild(hrsTitle);
+    hrsContainer.appendChild(hrsLine1);
+    hrsContainer.appendChild(hrsLine2);
+    hrsContainer.appendChild(hrsLine3);
+    hrsContainer.appendChild(hrsLine4);
+
+}
